33d112f4179f62247ed7aec75b9be090298fc338
Alexia Barnes <alexiarbarnes@gmail.com>
2026-01-26 17:38:06 -0500

    Added Create_World() function.

--- generate.py
+++ generate.py
@@ -1,6 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_World():
+	pyrosim.Start_SDF("world.sdf")
+	
+	# Create a link in world
+	pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+
+	pyrosim.End()
 
 # Set link size vars
 length = 1
@@ -12,20 +18,4 @@ x = 0
 y = 0
 z = 0.5
 
-# Creates a tower of 10 links where each one has 90% the l, w, h of the one below it
-
-for i in range(5):
-	x = i
-	for j in range(5):
-		y = j
-		length = 1
-		width = 1
-		height = 1
-		
-		for k in range(10):
-			pyrosim.Send_Cube(name="Box", pos=[x, y, z + k], size=[length, width, height])
-			length = length * 0.9
-			width = width * 0.9
-			height = height * 0.9
-
-pyrosim.End()
+Create_World()
