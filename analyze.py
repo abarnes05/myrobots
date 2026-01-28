bff525a4d3fd70dcd0b5becfb79f6846fad20fa0
Alexia Barnes <alexiarbarnes@gmail.com>
2026-01-27 19:23:11 -0500

    Loaded sensor values from file and visualized using matplotlib.

---
+++ analyze.py
@@ -0,0 +1,9 @@
+import numpy
+import matplotlib.pyplot
+
+backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+print(backLegSensorValues)
+
+matplotlib.pyplot.plot(backLegSensorValues)
+
+matplotlib.pyplot.show()
