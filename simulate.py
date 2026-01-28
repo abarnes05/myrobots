8912ceec6993534c5eaffce8e401bb75887bfef2
Alexia Barnes <alexiarbarnes@gmail.com>
2026-01-27 19:21:34 -0500

    Added a back leg sensor and saved sensor values to a file.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 
@@ -21,9 +23,19 @@ p.loadSDF("world.sdf")
 # Add robot
 robotId = p.loadURDF("body.urdf")
 
+# Setting up pyrosim
+pyrosim.Prepare_To_Simulate(robotId)
+
+# Create vector to store sensor values
+backLegSensorValues = numpy.zeros(1000)
+
 for i in range(1000):
 	time.sleep(1/60)
-	print(i)
 	p.stepSimulation()
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
+# Save sensor values to file
+numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
 
 p.disconnect()
+print(backLegSensorValues)
