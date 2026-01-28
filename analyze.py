039b9becd7e6896ecd7a85b239856aba4354e938
Alexia Barnes <alexiarbarnes@gmail.com>
2026-01-27 19:54:01 -0500

    Loaded front leg sensor values from file and added them to graph w/ back leg values. Added a legend to graph.

--- analyze.py
+++ analyze.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot
 backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
 print(backLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues)
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+print(frontLegSensorValues)
 
+matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg", linewidth = 2)
+matplotlib.pyplot.plot(frontLegSensorValues, label = "Front Leg")
+
+matplotlib.pyplot.legend(bbox_to_anchor=(0.5, 1.15), loc="upper center")
 matplotlib.pyplot.show()
