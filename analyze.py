dd431311d203ddda75149208f31908e2f26c8025
Alexia Barnes <alexiarbarnes@gmail.com>
2026-02-04 12:31:19 -0500

    Plotted back leg and front leg target angle values.

--- analyze.py
+++ analyze.py
@@ -7,8 +7,14 @@ print(backLegSensorValues)
 frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
 print(frontLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg", linewidth = 2)
-matplotlib.pyplot.plot(frontLegSensorValues, label = "Front Leg")
+backLegTargetAngles = numpy.load("data/backLegTargetAngles.npy")
+frontLegTargetAngles = numpy.load("data/frontLegTargetAngles.npy")
 
-matplotlib.pyplot.legend(bbox_to_anchor=(0.5, 1.15), loc="upper center")
+matplotlib.pyplot.plot(backLegTargetAngles)
+matplotlib.pyplot.plot(frontLegTargetAngles)
+
+# matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg", linewidth = 2)
+# matplotlib.pyplot.plot(frontLegSensorValues, label = "Front Leg")
+
+# matplotlib.pyplot.legend(bbox_to_anchor=(0.5, 1.15), loc="upper center")
 matplotlib.pyplot.show()
