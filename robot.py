7cd58736d4205054f7302e2439f80b2078a5f400
Alexia Barnes <alexiarbarnes@gmail.com>
2026-02-07 17:52:51 -0500

    Changed Act() method.

--- robot.py
+++ robot.py
@@ -1,14 +1,17 @@
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 from sensor import SENSOR
 from motor import MOTOR
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
 	def __init__(self, time_steps):
 		self.time_steps = time_steps
 		self.robotId = p.loadURDF("body.urdf")
+		self.nn = NEURAL_NETWORK("brain.nndf")
 		pyrosim.Prepare_To_Simulate(self.robotId)
 		self.Prepare_To_Sense()
 		self.Prepare_To_Act()
@@ -23,6 +26,10 @@ class ROBOT:
 		for i in self.sensors:
 			self.sensors[i].Get_Value(t)
 
+	def Think(self):
+		self.nn.Update()
+		self.nn.Print()
+
 	def Prepare_To_Act(self):
 		self.motors = {}
 
@@ -30,5 +37,9 @@ class ROBOT:
 			self.motors[jointName] = MOTOR(jointName, self.time_steps)
 
 	def Act(self, t):
-		for i in self.motors:
-			self.motors[i].Set_Value(self.robotId, t)
+		for neuronName in self.nn.Get_Neuron_Names():
+			if self.nn.Is_Motor_Neuron(neuronName):
+				jointName = self.nn.Get_Motor_Neurons_Joint(neuronName).encode("utf-8")
+				desiredAngle = self.nn.Get_Value_Of(neuronName)
+				self.motors[jointName].Set_Value(self.robotId, desiredAngle)
+				jointName = jointName.decode("utf-8")
