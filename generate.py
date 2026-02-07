4bd5c7d0ba279f179c8ebf19f9636d5cdab73f48
Alexia Barnes <alexiarbarnes@gmail.com>
2026-02-07 17:14:30 -0500

    Added Generate_Body() and Generate_Brain() methods.

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ def Create_World():
 	pyrosim.Send_Cube(name="Box", pos=[x - 3, y + 3, z], size=[length, width, height])
 	pyrosim.End()
 
-def Create_Robot():
+def Generate_Body():
 	pyrosim.Start_URDF("body.urdf")
 	# Torso
 	pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
@@ -18,7 +18,16 @@ def Create_Robot():
 	pyrosim.Send_Joint(name = "Torso_FrontLeg", parent = "Torso", child = "FrontLeg", type = "revolute", position = [2.0, 0, 1.0])
 	# FrontLeg
 	pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
+	pyrosim.End()
 
+def Generate_Brain():
+	pyrosim.Start_NeuralNetwork("brain.nndf")
+	pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+	pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+	pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+	
+	pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+	pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 	pyrosim.End()
 
 # Set link size vars
@@ -26,10 +35,6 @@ length = 1
 width = 1
 height = 1
 
-# Set link position vars
-x = 0
-y = 0
-z = 0.5
-
 Create_World()
-Create_Robot()
+Generate_Body()
+Generate_Brain()
