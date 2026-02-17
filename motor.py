82eed57233a013c002a880d2623b14eb9b4a1c35
Alexia Barnes <alexiarbarnes@gmail.com>
2026-02-17 10:55:34 -0500

    Removed self.time_steps.

--- motor.py
+++ motor.py
@@ -3,13 +3,12 @@ import pyrosim.pyrosim as pyrosim
 import pybullet_data
 
 class MOTOR:
-	def __init__(self, jointName, time_steps):
+	def __init__(self, jointName):
 		self.jointName = jointName
-		self.time_steps = time_steps
 
-	def Set_Value(self, robotId, desiredAngle):
+	def Set_Value(self, robot, desiredAngle):
 		pyrosim.Set_Motor_For_Joint(
-			bodyIndex = robotId, 
+			bodyIndex = robot, 
 			jointName = self.jointName, 
 			controlMode = p.POSITION_CONTROL, 
 			targetPosition = desiredAngle, 
