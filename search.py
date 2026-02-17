a46beb31c433b9ff6a4a5dc35c0fdee0a0440318
Alexia Barnes <alexiarbarnes@gmail.com>
2026-02-17 18:45:56 -0500

    Called new Show_Best() function.

--- search.py
+++ search.py
@@ -3,4 +3,7 @@ from hillclimber import HILL_CLIMBER
 
 # Create a HILL_CLIMBER object
 hc = HILL_CLIMBER()
+# Run the hill climber simulation
 hc.Evolve()
+# Visually displays the last generation's parent (the best fitness)
+hc.Show_Best()
