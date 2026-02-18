25dd9820b64037334fa384f97acf75f25701b84c
Alexia Barnes <alexiarbarnes@gmail.com>
2026-02-17 19:26:08 -0500

    Called new function.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,11 @@
+import sys
 from simulation import SIMULATION
 
+directOrGUI = sys.argv[1]
+
 # Create a simulation object
-simulation = SIMULATION()
+simulation = SIMULATION(directOrGUI)
 # Call Run() to run the simulation
 simulation.Run()
+
+simulation.Get_Fitness()
