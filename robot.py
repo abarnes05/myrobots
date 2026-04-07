dd1a17e88f238d04d4d921620c7db65ef4412662
Alexia Barnes <alexiarbarnes@gmail.com>
2026-04-06 20:53:36 -0400

    Some housekeeping.

--- robot.py
+++ robot.py
@@ -1,3 +1,4 @@
+import constants as c
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
@@ -11,7 +12,6 @@ from pyrosim.neuralNetwork import NEURAL_NETWORK
 class ROBOT:
 	def __init__(self, solutionID):
 		self.robot = p.loadURDF("body.urdf")
-		# self.solutionID = solutionID
 		self.nn = NEURAL_NETWORK(f"brain{solutionID}.nndf")
 		pyrosim.Prepare_To_Simulate(self.robot)
 		self.Prepare_To_Sense()
@@ -48,7 +48,7 @@ class ROBOT:
 				# Save the name of motor neuron's joint
 				jointName = self.nn.Get_Motor_Neurons_Joint(neuronName).encode("utf-8")
 				# Set the desiredAngle to the value of motor neuron
-				desiredAngle = self.nn.Get_Value_Of(neuronName)
+				desiredAngle = self.nn.Get_Value_Of(neuronName) * c.motorJointRange
 				# Update the motorized joint value so it will apply torque 
 				# to it's links based on desiredAngle
 				self.motors[jointName].Set_Value(self.robot, desiredAngle)
@@ -56,13 +56,21 @@ class ROBOT:
 	
 	def Get_Fitness(self, solutionID):
 		# Get the state of link 0
-		stateOfLinkZero = p.getLinkState(self.robot, 0)
+		# stateOfLinkZero = p.getLinkState(self.robot, 0)
 		# Get the position of link 0
-		positionOfLinkZero = stateOfLinkZero[0]
+		# positionOfLinkZero = stateOfLinkZero[0]
 		# Get the x-coord of link 0
-		xCoordinateOfLinkZero = positionOfLinkZero[0]
+		# xCoordinateOfLinkZero = positionOfLinkZero[0]
+
+		# Get the position and orientation of the torso
+		basePositionAndOrientation = p.getBasePositionAndOrientation(self.robot)
+		# Get the position of the torso
+		basePosition = basePositionAndOrientation[0]
+		# Get the horizontal pos of the torso
+		xPosition = basePosition[0]
 		
 		# Write the x-coord of link 0 to a file
 		with open(f"tmp{solutionID}.txt", "w") as fitnessFile:
-			fitnessFile.write(str(xCoordinateOfLinkZero))
+			fitnessFile.write(str(xPosition))
+		
 		os.system(f"mv tmp{solutionID}.txt fitness{solutionID}.txt")
