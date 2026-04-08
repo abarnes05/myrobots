83d26aef2bbef8d2a36f2a7e778c1998f2e145fd
Alexia Barnes <alexiarbarnes@gmail.com>
2026-04-08 15:23:04 -0400

    Temporarily commented out things.

--- simulate.py
+++ simulate.py
@@ -1,12 +1,12 @@
 import sys
 from simulation import SIMULATION
 
-directOrGUI = sys.argv[1]
-solutionID = sys.argv[2]
+# directOrGUI = sys.argv[1]
+# solutionID = sys.argv[2]
 
 # Create a simulation object
-simulation = SIMULATION(directOrGUI, solutionID)
+simulation = SIMULATION()
 # Call Run() to run the simulation
 simulation.Run()
 
-simulation.Get_Fitness(solutionID)
+# simulation.Get_Fitness(solutionID)
