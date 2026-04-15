6e4ab18e93ff28f73536ed0f4704822a7bb25290
Alexia Barnes <alexiarbarnes@gmail.com>
2026-04-15 10:29:00 -0400

    Uncommented things out. Changed fitness function.

--- robot.py
+++ robot.py
@@ -1,7 +1,6 @@
 import constants as c
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
-import numpy
 import os
 
 from sensor import SENSOR
@@ -9,19 +8,19 @@ from motor import MOTOR
 from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
-	def __init__(self):
+	def __init__(self, solutionID, ballID):
 		self.robot = p.loadURDF("body.urdf", basePosition=[0,0,0], useFixedBase=True)
-		# self.nn = NEURAL_NETWORK(f"brain{solutionID}.nndf")
+		self.nn = NEURAL_NETWORK(f"brain{solutionID}.nndf")
+		self.ball = ballID
 		pyrosim.Prepare_To_Simulate(self.robot)
 		self.Prepare_To_Sense()
-		# self.Prepare_To_Act()
-		# os.system(f"rm brain{solutionID}.nndf")
+		self.Prepare_To_Act()
+		os.system(f"rm brain{solutionID}.nndf")
 
 	def Prepare_To_Sense(self):
 		self.sensors = {}
 
-		for linkName in pyrosim.linkNamesToIndices:
-			self.sensors[linkName] = SENSOR(linkName)
+		self.sensors["Hand"] = SENSOR("Hand")
 
 	def Sense(self, t):
 		for i in self.sensors:
@@ -39,40 +38,30 @@ class ROBOT:
 		for jointName in pyrosim.jointNamesToIndices:
 			self.motors[jointName] = MOTOR(jointName)
 
-	def Act(self, t):
-		for i in self.motors:
-			self.motors[i].Set_Value(self.robot, t)
-		
+	def Act(self):
 		# For every neuron in the neural network
-		# for neuronName in self.nn.Get_Neuron_Names():
-		# 	# Check if it's a motor neuron
-		# 	if self.nn.Is_Motor_Neuron(neuronName):
-		# 		# Save the name of motor neuron's joint
-		# 		jointName = self.nn.Get_Motor_Neurons_Joint(neuronName).encode("utf-8")
-		# 		# Set the desiredAngle to the value of motor neuron
-		# 		desiredAngle = self.nn.Get_Value_Of(neuronName) * c.motorJointRange
-		# 		# Update the motorized joint value so it will apply torque 
-		# 		# to it's links based on desiredAngle
-		# 		self.motors[jointName].Set_Value(self.robot, desiredAngle)
-		# 		jointName = jointName.decode("utf-8")
+		for neuronName in self.nn.Get_Neuron_Names():
+			# Check if it's a motor neuron
+			if self.nn.Is_Motor_Neuron(neuronName):
+				# Save the name of motor neuron's joint
+				jointName = self.nn.Get_Motor_Neurons_Joint(neuronName).encode("utf-8")
+				# Set the desiredAngle to the value of motor neuron
+				desiredAngle = self.nn.Get_Value_Of(neuronName) * c.motorJointRange
+				# Update the motorized joint value so it will apply torque 
+				# to it's links based on desiredAngle
+				self.motors[jointName].Set_Value(self.robot, desiredAngle)
+				jointName = jointName.decode("utf-8")
 	
 	def Get_Fitness(self, solutionID):
-		# Get the state of link 0
-		# stateOfLinkZero = p.getLinkState(self.robot, 0)
-		# Get the position of link 0
-		# positionOfLinkZero = stateOfLinkZero[0]
-		# Get the x-coord of link 0
-		# xCoordinateOfLinkZero = positionOfLinkZero[0]
-
-		# Get the position and orientation of the torso
-		basePositionAndOrientation = p.getBasePositionAndOrientation(self.robot)
-		# Get the position of the torso
-		basePosition = basePositionAndOrientation[0]
-		# Get the horizontal pos of the torso
-		xPosition = basePosition[0]
+		# Get the position and orientation of the ball
+		ballPositionAndOrientation = p.getBasePositionAndOrientation(self.ball)
+		# Get the position of the ball
+		ballPosition = ballPositionAndOrientation[0]
+		# Get the horizontal pos of the ball
+		yPosition = ballPosition[1]
 		
-		# Write the x-coord of link 0 to a file
+		# Write the y-coord of ball to a file
 		with open(f"tmp{solutionID}.txt", "w") as fitnessFile:
-			fitnessFile.write(str(xPosition))
+			fitnessFile.write(str(yPosition))
 		
 		os.system(f"mv tmp{solutionID}.txt fitness{solutionID}.txt")
