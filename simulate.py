9970a583df7e4fc633adda9dfa2c3431cb4e2288
Alexia Barnes <alexiarbarnes@gmail.com>
2026-04-24 11:37:40 -0400

    Added two ways to run the simulation and get the fitness of the robot, based on the different fitness functions.

--- simulate.py
+++ simulate.py
@@ -6,7 +6,12 @@ solutionID = sys.argv[2]
 
 # Create a simulation object
 simulation = SIMULATION(directOrGUI, solutionID)
+
 # Call Run() to run the simulation
+# landingY = simulation.Run()
+
+# simulation.Get_Fitness(solutionID, landingY)
+
 simulation.Run()
 
-simulation.Get_Fitness(solutionID)
+simulation.Get_Fitness(solutionID, simulation.world.yPositions, simulation.world.zPositions)
