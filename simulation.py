f840ae510b40bfa76d878c93717ecc41d213649f
Alexia Barnes <alexiarbarnes@gmail.com>
2026-04-24 11:38:17 -0400

    Added two ways to run the simulation and get the fitness of the robot, based on the different fitness functions.

--- simulation.py
+++ simulation.py
@@ -21,7 +21,33 @@ class SIMULATION:
 		# Initialize WORLD object
 		self.world = WORLD()
 		# Initialize ROBOT object
-		self.robot = ROBOT(solutionID, self.world.ballID)
+		self.robot = ROBOT(solutionID)
+		
+		# self.ballHitGround = False
+		# self.landingY = 0
+	
+	# def Run(self):
+	# 	for t in range(c.numTimeSteps):
+	# 		if self.directOrGUI == "GUI":
+	# 			time.sleep(1/60)
+	# 		# Step simulation at every time-step
+	# 		p.stepSimulation()
+	# 		# Record ball position every time-step
+	# 		ballPos = self.world.Get_Ball_Position()
+	# 		if not self.ballHitGround:
+	# 			# If the z-pos of the ball is <= to ball radius
+	# 			if ballPos[2] <= 0.25:
+	# 				# Then the ball has hit the ground
+	# 				self.ballHitGround = True
+	# 				self.landingY = ballPos[1]
+	# 		# Call Sense() to get sensor values
+	# 		self.robot.Sense(t)
+	# 		# Call Think() to update and print neuron values
+	# 		self.robot.Think()
+	# 		# Call Act() to have motors move the robot's joints based on the motor neurons' values
+	# 		self.robot.Act()
+		
+	# 	return self.landingY
 	
 	def Run(self):
 		for t in range(c.numTimeSteps):
@@ -29,6 +55,8 @@ class SIMULATION:
 				time.sleep(1/60)
 			# Step simulation at every time-step
 			p.stepSimulation()
+			# Record ball position every time-step
+			self.world.Get_Ball_Position(t)
 			# Call Sense() to get sensor values
 			self.robot.Sense(t)
 			# Call Think() to update and print neuron values
@@ -36,13 +64,11 @@ class SIMULATION:
 			# Call Act() to have motors move the robot's joints based on the motor neurons' values
 			self.robot.Act()
 	
-	def Get_Fitness(self, solutionID):
-		self.robot.Get_Fitness(solutionID)
+	# def Get_Fitness(self, solutionID, landingY):
+	# 	self.robot.Get_Fitness(solutionID, landingY)
+
+	def Get_Fitness(self, solutionID, ballYPositions, ballZPositions):
+		self.robot.Get_Fitness(solutionID, ballYPositions, ballZPositions)
 
 	def __del__(self):
 		p.disconnect()
-	
-	
-        
-        
-	    
