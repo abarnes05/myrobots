4c975ef32065051a1a7ae9f00c913c549cc1bee5
Alexia Barnes <alexiarbarnes@gmail.com>
2026-04-24 11:36:02 -0400

    Added two versions of new method to get the ball position coords. One is used for fitness = landingY pos and the other one is for fitness = finalY pos or fitness = finalY + maxZ pos

--- world.py
+++ world.py
@@ -1,7 +1,25 @@
+import constants as c
 import pybullet as p
+import numpy
 
 class WORLD:
 	def __init__(self):
 		self.planeID = p.loadURDF("plane.urdf")
-		self.worldID = p.loadSDF("world.sdf")
-		self.ballID = self.worldID[0]
+		self.objects = p.loadSDF("world.sdf")
+		self.ballID = self.objects[0]
+		
+		# Create vectors to store position values
+		self.xPositions = numpy.zeros(c.numTimeSteps)
+		self.yPositions = numpy.zeros(c.numTimeSteps)
+		self.zPositions = numpy.zeros(c.numTimeSteps)
+
+	# def Get_Ball_Position(self):
+	# 	position = p.getBasePositionAndOrientation(self.ballID)[0]
+
+	# 	return position
+	
+	def Get_Ball_Position(self, t):
+		position = p.getBasePositionAndOrientation(self.ballID)[0]
+		
+		self.yPositions[t] = position[1]
+		self.zPositions[t] = position[2]
